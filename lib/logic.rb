2d1065a1af4d06f6c1b7e98da18060da3c0b20b2
enaburekhan <ericenaburekhan2018@gmail.com>
2020-10-28 13:48:35 +0100

    add top level comments

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,35 +1,3 @@
-# winning_comb = [
-#  [1, 2, 3], 
-#  [4, 5, 6], 
-#  [7, 8, 9], 
-#  [3, 5, 7], 
-#  [1, 5, 9], 
-#  [1, 4, 7], 
-#  [2, 5, 8], 
-#  [3, 6, 9]
-# ]
-
-# class Logic
-#   def game_board(board_states) 
-#     states = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [3, 5, 7], [1, 5, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9]]
-#     if states.include?(board1)
-#       board1.each { |state| board_states[state - 1] = 'X' }
-#     #   puts "#{player1} win"
-#     # # break
-#     end
-#   end
-  
-#    def game_board(board_states)
-#      states = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [3, 5, 7], [1, 5, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9]]
-#      unless states.include?(board2)     
-#         board2.each { |state| board_states[state - 1] = 'X' }
-#         # puts "#{player2} win"
-#      end
-#     #  break
-#    end
-
-# end
-
 # logic class
 class Logic
   attr_reader :player_position
@@ -58,12 +26,13 @@ class Position
   end
 end
 
+# Array operation class
 class ArrayOperation
   def initialize(arr, pos)
     @arr = arr
     @pos = pos
   end
-  
+
   def delete_arr
     @arr.delete(@pos)
   end
