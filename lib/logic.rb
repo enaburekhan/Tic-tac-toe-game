894e5e0426d85f2beb137167cbc1e8d7ca71e9ad
enaburekhan <ericenaburekhan2018@gmail.com>
2020-10-28 14:19:47 +0100

    fix linter errors

--- lib/logic.rb
+++ lib/logic.rb
@@ -4,14 +4,14 @@ class Logic
   def initialize(player_position)
     @player_position = player_position
   end
-  
+
   def winning_position?
-    arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]] 
+    arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
     arr.any? do |combination|
       return true if combination.all? { |element| @player_position.include?(element) }
     end
-    false    
-  end 
+    false
+  end
 end
 
 # logic defines player's position on the board
@@ -20,8 +20,8 @@ class Position
   def initialize(choice, board)
     @choice = choice
     @board  = board
-  end  
-  
+  end
+
   def inside
     @board.include?(@choice)
   end
@@ -38,5 +38,3 @@ class ArrayOperation
     @arr.delete(@pos)
   end
 end
-
-
