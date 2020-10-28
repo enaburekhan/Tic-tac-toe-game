75ca1e7d37fe78f2acab4d7163d01cd5f8e92718
enaburekhan <ericenaburekhan2018@gmail.com>
2020-10-28 13:42:14 +0100

    refactor class logic

--- lib/logic.rb
+++ lib/logic.rb
@@ -32,38 +32,41 @@
 
 # logic class
 class Logic
-  attr_reader :arr, :player_position, :board
-  @@arr =[[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]] 
+  attr_reader :player_position
   def initialize(player_position)
-      @player_position = player_position
+    @player_position = player_position
   end
   
   def winning_position?
-    @@arr.any? do |combination|
+    arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]] 
+    arr.any? do |combination|
       return true if combination.all? { |element| @player_position.include?(element) }
     end
     false    
   end 
 end
 
+# class position
 class Position
-  attr_accessor :choice
+  attr_accessor :choice, :board
   def initialize(choice, board)
-      @choice = choice
-      @board  = board
+    @choice = choice
+    @board  = board
   end  
-  def inside?
-      @board.include?(@choice)
+  def inside
+    @board.include?(@choice)
   end
 end
 
 class ArrayOperation
-  def initialize(arr,pos)
-      @arr = arr
-      @pos = pos
+  def initialize(arr, pos)
+    @arr = arr
+    @pos = pos
   end
-  def to_deleteArr
-      @arr.delete(@pos)
+  
+  def delete_arr
+    @arr.delete(@pos)
   end
 end
+
 
